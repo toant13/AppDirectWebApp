a9a4ad54356d0bcfc4c2b85d405db498d103dc68
Tony Tran <toant713@gmail.com>
2014-07-26 13:36:43 -0700

    Updated produce type for post calls

--- src/main/java/com/app/dir/controller/EventController.java
+++ src/main/java/com/app/dir/controller/EventController.java
@@ -7,49 +7,52 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import com.app.dir.domain.Event;
 import com.app.dir.domain.EventResult;
 import com.app.dir.event.EventHandler;
 
-
-
-
-
-
 @Controller
 @RequestMapping("/")
 public class EventController {
 	final private EventHandler eventHandler = new EventHandler();
-	
-	
+
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public String orderSubscription(@RequestParam(value="url", required=true) String token, ModelMap model) {
-		
-		//1) extract url
-		//2) call get to url
+	public @ResponseBody EventResult orderSubscription(
+			@RequestParam(value = "url", required = true) String token,
+			ModelMap model) {
+
+		// 1) extract url
+		// 2) call get to url
 		// *handler error here
-		//3) call event handler with xml from url
-		//4) get EventResult, post back to appdirect
-		
+		// 3) call event handler with xml from url
+		// 4) get EventResult, post back to appdirect
+
 		RestTemplate template = new RestTemplate();
-		
+
 		Event event = eventHandler.getEvent(template, token);
 		EventResult eventResult = eventHandler.processEvent(event);
-		
-		eventHandler.sendEventResults(template, event.getReturnUrl(), eventResult);
-		
-		model.addAttribute("message", "Great success: " + eventResult.getMessage());		
-		return "index";
-	
+
+		eventHandler.sendEventResults(template, event.getReturnUrl(),
+				eventResult);
+
+		model.addAttribute("message",
+				"Great success: " + eventResult.getMessage());
+		return eventResult;
 	}
-	
+
 	@RequestMapping(value = "/post/{name}", method = RequestMethod.POST)
-	public String welcomeName(@PathVariable String name, ModelMap model) {
+	public  @ResponseBody EventResult welcomeName(@PathVariable String name, ModelMap model) {
 
-		model.addAttribute("message", "TESTING POST: " + name);
-		return "index";
+		EventResult eventResult = new EventResult();
 
-	}	
+		eventResult.setSuccess(true);
+		eventResult.setMessage("Account creation successful");
+		eventResult.setAccountIdentifier("new-account-identifier");
+		
+		return eventResult;
+
+	}
 }
